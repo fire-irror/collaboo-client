2f4ece49f4ae9699566af62e32ee350c32bfb51e
fire-irror <firenoah.m@gmail.com>
2024-03-26 21:02:52 +0900

    feat: 회원가입 화면

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 import Signinup from './Signinup/Signinup'
 import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
 
   function App() {
   return (
@@ -8,6 +9,7 @@ import Login from './Login/Login';
       <Routes>
         <Route path='/' element={<Signinup/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
       </Routes>
     </Router>
   );
--- src/Signinup/Signinup.jsx
+++ src/Signinup/Signinup.jsx
@@ -12,7 +12,7 @@ export default function Login(){
   }
 
   const handleSignUpClick = () =>{
-    navigate('/');
+    navigate('/signup');
   }
 
   return(
