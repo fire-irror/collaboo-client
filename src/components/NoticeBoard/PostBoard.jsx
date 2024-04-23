9fffcc141bc970ca0946b7a94b30267b28c50e8b
fire-irror <firenoah.m@gmail.com>
2024-04-23 13:11:20 +0900

    fix : post css수정

--- src/components/NoticeBoard/PostBoard.jsx
+++ src/components/NoticeBoard/PostBoard.jsx
@@ -12,7 +12,6 @@ export default function PostBoard() {
 
   const handlePostBoardCreate = async () => {
     const PostedBoard = {
-      // id: null,
       title: title,
       category: category,
       code: code,
